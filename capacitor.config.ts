641362d13ba1e6e19244729e844ae01958140d27
GaloszJakub <kubagalosz@gmail.com>
2026-05-13 21:47:04 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.archiwum.app',
+  appId: 'archiwum.app',
   appName: 'Archiwum',
   webDir: 'dist',
   server: {
